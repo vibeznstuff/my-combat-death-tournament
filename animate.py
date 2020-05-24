66f176f54ee8831c304c995b3eb6fde6ec9a00f8
vibeznstuff <richard.dansoh@gmail.com>
2020-05-24 15:50:46 -0400

    Added defeat and victory animations, also added additional slow factors in config file for certain animations by character type

--- animate.py
+++ animate.py
@@ -13,8 +13,30 @@ mappings = json.load(open("animation_mappings.json"))
 
 print(mappings)
 
-player_one = 'yuri'
-player_two = 'iori'
+player_one = 'samurai_male'
+player_two = 'hero_male'
+
+p1_health_max = 100
+p2_health_max = 100
+p1_health = p1_health_max
+p2_health = p2_health_max
+# 350 Max
+
+p1_dodge_space = mappings[player_one]['dodge_space']
+p2_dodge_space = mappings[player_two]['dodge_space']
+
+p1_dodge_sf = mappings[player_one]['dodge_slow_factor']
+p2_dodge_sf = mappings[player_two]['dodge_slow_factor']
+
+p1_defeat_sf = mappings[player_one]['defeat_slow_factor']
+p2_defeat_sf = mappings[player_two]['defeat_slow_factor']
+
+p1_victory_sf = mappings[player_one]['victory_slow_factor']
+p2_victory_sf = mappings[player_two]['victory_slow_factor']
+
+p1_dodge_rate = 0
+p2_dodge_rate = 0
+
 
 x = -90
 x_default = x
@@ -22,8 +44,6 @@ y = 135
 x_2 = 445
 x_2_default = x_2
 y_2 = 130
-width = 60
-height = 20
 velocity = 5
 
 FRAME_RATE = 35
@@ -60,7 +80,7 @@ def get_frames(character, action, flip_bool=False, p1_bool=True, slow_factor=0):
         else:
             step = 1
 
-        for i in range(ani_range[0], ani_range[1], step):
+        for i in range(ani_range[0], ani_range[1] + 1, step):
             id = "_{}".format(str(i))
 
             tmp_slow_factor = slow_factor
@@ -106,21 +126,34 @@ def draw():
     global p2_dodging
     global p1_resting
     global p2_resting
+    global p1_health_max
+    global p2_health_max
+    global p1_health
+    global p2_health
+    global p1_defeat
+    global p2_defeat
+    global p1_victory
+    global p2_victory
 
-    if p1_step_count + 1 >= p1_limit:
+    if p1_step_count + 1 >= p1_limit and not (p1_defeat or p1_victory):
         p1_step_count = 0
         p1_attacking = False
-        p1_recoiling = False
+        if p1_recoiling and p2_resting:
+            p1_health = max(0, p1_health - 50)
+            p1_recoiling = False
         p2_dodging = False
 
-    if p2_step_count + 1 >= p2_limit:
+    if p2_step_count + 1 >= p2_limit and not (p2_defeat or p2_victory):
         p2_step_count = 0
         p2_attacking = False
-        p2_recoiling = False
+        if p2_recoiling and p1_resting:
+            p2_health = max(0, p2_health - 50)
+            p2_recoiling = False
         p1_dodging = False
     
     win.fill((0,0,0))
     win.blit(swanky_background,(0,0))
+
     if p1_attacking and not p2_attacking:
         win.blit(p2_frames[p2_step_count//3], (x_2, y_2))
         win.blit(p1_frames[p1_step_count//3], (x, y))
@@ -130,6 +163,22 @@ def draw():
     else:
         win.blit(p2_frames[p2_step_count//3], (x_2, y_2))
         win.blit(p1_frames[p1_step_count//3], (x, y)) 
+
+
+    # Draw health bar boarders
+    pygame.draw.rect(win, (255, 255, 255), (50-2, 75-2, p1_health_max+4, 30+4))
+    pygame.draw.rect(win, (255, 255, 255), (1000 - p2_health_max - 50 - 2, 75-2, p2_health_max+4, 30+4))
+
+    # Draw red health bar background
+    pygame.draw.rect(win, (255, 0, 0), (50, 75, p1_health_max, 30))
+    pygame.draw.rect(win, (255, 0, 0), (1000 - p2_health_max - 50, 75, p2_health_max, 30))
+
+    # Draw green health bar foreground
+    if p1_health > 0:
+        pygame.draw.rect(win, (0, 255, 0), (50, 75, p1_health, 30))
+
+    if p2_health > 0:
+        pygame.draw.rect(win, (0, 255, 0), (1000 - p2_health - 50, 75, p2_health, 30))
 
     pygame.display.update()
 
@@ -149,6 +198,10 @@ p1_dodging = False
 p1_dodged = False
 p2_dodging = False
 p2_dodged = False
+p1_defeat = False
+p2_defeat = False
+p1_victory = False
+p2_victory = False
 acc = 0
 acc_2 = 0
 
@@ -156,13 +209,35 @@ while run:
 
     clock.tick(FRAME_RATE)
 
-    if not p1_resting and not p1_attacking and not p1_recoiling and not p1_dodging:
+    if p2_health == 0 and not p2_defeat:
+        p2_defeat = True
+        p2_attacking = False
+        p2_dodging = False
+        p2_resting = False
+        p2_frames = get_frames(character=player_two, action='defeat', flip_bool=False, p1_bool=False, slow_factor=p2_defeat_sf)
+        p2_step_count = 0
+        p1_frames = get_frames(character=player_one, action='victory', flip_bool=True, p1_bool=True, slow_factor=p1_victory_sf)
+        p1_step_count = 0
+        p1_victory = True
+
+    if p1_health == 0 and not p1_defeat:
+        p1_defeat = True
+        p1_attacking = False
+        p1_dodging = False
+        p1_resting = False
+        p1_frames = get_frames(character=player_one, action='defeat', flip_bool=True, p1_bool=True, slow_factor=p1_defeat_sf)
+        p1_step_count = 0
+        p2_frames = get_frames(character=player_two, action='victory', flip_bool=False, p1_bool=False, slow_factor=p2_victory_sf)
+        p2_step_count = 0
+        p2_victory = True
+
+    if not p1_resting and not p1_attacking and not p1_recoiling and not p1_dodging and not p1_defeat and not p1_victory:
         p1_frames = get_frames(character=player_one, action='rest', flip_bool=True, p1_bool=True)
         p1_step_count = 0
         p1_resting = True
         x = -90
 
-    if not p2_resting and not p2_attacking and not p2_recoiling and not p2_dodging:
+    if not p2_resting and not p2_attacking and not p2_recoiling and not p2_dodging and not p2_defeat and not p2_victory:
         p2_frames = get_frames(character=player_two, action='rest', flip_bool=False, p1_bool=False)
         p2_step_count = 0
         p2_resting = True
@@ -184,7 +259,7 @@ while run:
         acc_2 = 0
 
     if (x_2 - x) < 350 and p1_attacking and not p2_recoiling and p2_dodged and not p2_dodging:
-        p2_frames = get_frames(character=player_two, action='dodge', flip_bool=False, p1_bool=False, slow_factor=2)
+        p2_frames = get_frames(character=player_two, action='dodge', flip_bool=False, p1_bool=False, slow_factor=p2_dodge_sf)
         p2_step_count = 0
         p2_resting = False
         p2_dodged = False
@@ -192,11 +267,11 @@ while run:
         acc_2 = 0
 
     if p2_dodging and p2_step_count > 0:
-        if x_2 > x_2_default + 80:
+        if x_2 > x_2_default + p2_dodge_space:
             p2_frames = get_frames(character=player_two, action='rest', flip_bool=False, p1_bool=False)
             p2_step_count = 0
-            x_2 = x_2_default + 80
-        elif x_2 < x_2_default + 80:
+            x_2 = x_2_default + p2_dodge_space
+        elif x_2 < x_2_default + p2_dodge_space:
             x_2 += (p2_step_count / p2_limit) * 10
 
     if p1_attacked:
@@ -206,7 +281,7 @@ while run:
         p1_attacking = True
         p1_attacked = False
 
-        if uniform(0,1) > 0.5:
+        if uniform(0,1) < p2_dodge_rate:
             p2_dodged = True
     
     if p2_attacking and not p2_recoiling:
@@ -225,7 +300,7 @@ while run:
         acc = 0
 
     if (x_2 - x) < 350 and p2_attacking and not p1_recoiling and p1_dodged and not p1_dodging:
-        p1_frames = get_frames(character=player_one, action='dodge', flip_bool=True, p1_bool=True, slow_factor=2)
+        p1_frames = get_frames(character=player_one, action='dodge', flip_bool=True, p1_bool=True, slow_factor=p1_dodge_sf)
         p1_step_count = 0
         p1_resting = False
         p1_dodged = False
@@ -233,11 +308,11 @@ while run:
         acc = 0
 
     if p1_dodging and p1_step_count > 0:
-        if x < x_default - 80:
+        if x < x_default - p1_dodge_space:
             p1_frames = get_frames(character=player_one, action='rest', flip_bool=True, p1_bool=True)
             p1_step_count = 0
-            x = x_default - 80
-        elif x > x_default - 80:
+            x = x_default - p1_dodge_space
+        elif x > x_default - p1_dodge_space:
             x -= (p1_step_count / p1_limit) * 10
 
     if p2_attacked:
@@ -247,7 +322,7 @@ while run:
         p2_attacking = True
         p2_attacked = False
 
-        if uniform(0,1) > 0.5:
+        if uniform(0,1) < p1_dodge_rate:
             p1_dodged = True
 
     for event in pygame.event.get():
@@ -269,8 +344,12 @@ while run:
         y += velocity
         print("x: {0}, y: {1}".format(x,y))
 
-    p1_step_count += 1
-    p2_step_count += 1
+    if not ((p1_defeat or p1_victory) and p1_step_count + 1 >= p1_limit):
+        p1_step_count += 1
+    
+    if not ((p2_defeat or p2_victory) and p2_step_count + 1 >= p2_limit):
+        p2_step_count += 1
+
     draw()
 
 pygame.quit()
