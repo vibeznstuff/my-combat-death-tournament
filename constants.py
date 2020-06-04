f9997a9b680166265e9f1bc8cc3d5ef5bcc290aa
vibeznstuff <richard.dansoh@gmail.com>
2020-06-04 18:05:55 -0400

    Added some comments to constants.py

--- constants.py
+++ constants.py
@@ -3,11 +3,25 @@ ROUND_NUMBER = 1
 
 # For N levels of tournament brackets fights, use 2 ** N for FIGHTER_COUNT
 FIGHTER_COUNT = 2 ** 5
+
+# How long the matches will last before time runs out. When the timer runs out,
+# the figher with the least health remaining with lose the fight
 MAX_FIGHT_MOMENTS = 300
+
+# Factor for scaling the default damage dealt of combatants
 DAMAGE_MULTIPLIER = 3
+
+# Factor for scaling the default health of combatants
 HEALTH_MULTIPLIER = 3
+
+# The default cool down steps for all combatants to perform an attack action
+# The actual cooldown for combatants will be this value minus their STAMINA value
 DEFAULT_COOLDOWN = 20
+
+# On a "Dodge" dice roll, probability of successfully dodging
 DODGE_THRESHOLD = 0.9
+
+# For stat bonus increases, the amount that a combatant's health will increase by
 HEALTH_INCREASE_BONUS = 1.5
 
 # Probability of combatant being ELITE rank
